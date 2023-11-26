2a712bd5fcfa74f3b91bbd1c13e8e9b8d8345fe8
dani-sundell <daniellesundell@gmail.com>
2023-11-26 15:58:59 -0800

    dist upload

--- client/src/components/ContentModalX.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="module--close">
-          <div class="module--hmbgr module--hmbgr__alt">
-            <i class="module--lines hmbgr" style="transform: translate(0rem, .3rem) rotate(45deg);"></i>
-            <i class="module--lines hmbgr" style="display: none;"></i>
-            <i class="module--lines hmbgr" style="transform: translate(0rem, -0.25rem) rotate(-45deg);"></i>
-          </div>
-    </div>
-</template>
